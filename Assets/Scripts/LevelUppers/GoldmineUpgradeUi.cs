a6c88b8ad49e60572fb8d4b7b9788f6eb14647a7
Arphail <Alexusoffd@gmail.com>
2023-08-02 16:26:29 +0300

    Fixed price goldmine

--- Assets/Scripts/LevelUppers/GoldmineUpgradeUi.cs
+++ Assets/Scripts/LevelUppers/GoldmineUpgradeUi.cs
@@ -14,13 +14,13 @@ public class GoldmineUpgradeUi : MonoBehaviour
     public void ShowStats(int currentLevel, int maxLevel, int currentCost)
     {
         if (YandexGamesSdk.Environment.i18n.lang == "en")
-            _currentLevelVisual.text = $"Level {currentLevel} / {maxLevel}";
+            _currentLevelVisual.text = $"Level\n{currentLevel} / {maxLevel}";
 
         if (YandexGamesSdk.Environment.i18n.lang == "ru")
-            _currentLevelVisual.text = $"Уровень {currentLevel} / {maxLevel}";
+            _currentLevelVisual.text = $"Уровень\n{currentLevel} / {maxLevel}";
 
         if (YandexGamesSdk.Environment.i18n.lang == "tr")
-            _currentLevelVisual.text = $"Seviye {currentLevel} / {maxLevel}";
+            _currentLevelVisual.text = $"Seviye\n{currentLevel} / {maxLevel}";
 
         _levelUpCost.text = currentCost.ToString();
 
